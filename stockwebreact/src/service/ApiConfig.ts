ca0753a13b34f86069bfeb80f1fee987555b719f
Kamal Vashist <115673060+kjvashist@users.noreply.github.com>
2023-12-13 17:04:54 -0500

    Delete stockwebreact directory
    Will checkin again

--- stockwebreact/src/service/ApiConfig.ts
+++
@@ -1,9 +0,0 @@
-const baseUrl = "http://localhost:8081/";
-
-const ApiConfig = {
-    //tickerprices: 'http://localhost:8081/tickerprices/'
-    tickerprices: baseUrl+'tickerprices',
-    tickers: baseUrl+'tickers'
-}
-
-export default ApiConfig;
